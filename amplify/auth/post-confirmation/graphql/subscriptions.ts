bc3bbb9618292eb688cea1660bf2fd0104e6dbaa
omesalles <37436831+omesalles@users.noreply.github.com>
2024-10-03 15:43:52 +0200

    Graphql created and used in Post-Confirmation trigger to check if household exists, if not create new household and add user to household

---
+++ amplify/auth/post-confirmation/graphql/subscriptions.ts
@@ -0,0 +1,133 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+import * as APITypes from "./API";
+type GeneratedSubscription<InputType, OutputType> = string & {
+  __generatedSubscriptionInput: InputType;
+  __generatedSubscriptionOutput: OutputType;
+};
+
+export const onCreateHousehold = /* GraphQL */ `subscription OnCreateHousehold($filter: ModelSubscriptionHouseholdFilterInput) {
+  onCreateHousehold(filter: $filter) {
+    createdAt
+    householdName
+    id
+    members {
+      nextToken
+      __typename
+    }
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedSubscription<
+  APITypes.OnCreateHouseholdSubscriptionVariables,
+  APITypes.OnCreateHouseholdSubscription
+>;
+export const onCreateUser = /* GraphQL */ `subscription OnCreateUser(
+  $filter: ModelSubscriptionUserFilterInput
+  $householdID: String
+) {
+  onCreateUser(filter: $filter, householdID: $householdID) {
+    createdAt
+    email
+    household {
+      createdAt
+      householdName
+      id
+      updatedAt
+      __typename
+    }
+    householdID
+    id
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedSubscription<
+  APITypes.OnCreateUserSubscriptionVariables,
+  APITypes.OnCreateUserSubscription
+>;
+export const onDeleteHousehold = /* GraphQL */ `subscription OnDeleteHousehold($filter: ModelSubscriptionHouseholdFilterInput) {
+  onDeleteHousehold(filter: $filter) {
+    createdAt
+    householdName
+    id
+    members {
+      nextToken
+      __typename
+    }
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedSubscription<
+  APITypes.OnDeleteHouseholdSubscriptionVariables,
+  APITypes.OnDeleteHouseholdSubscription
+>;
+export const onDeleteUser = /* GraphQL */ `subscription OnDeleteUser(
+  $filter: ModelSubscriptionUserFilterInput
+  $householdID: String
+) {
+  onDeleteUser(filter: $filter, householdID: $householdID) {
+    createdAt
+    email
+    household {
+      createdAt
+      householdName
+      id
+      updatedAt
+      __typename
+    }
+    householdID
+    id
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedSubscription<
+  APITypes.OnDeleteUserSubscriptionVariables,
+  APITypes.OnDeleteUserSubscription
+>;
+export const onUpdateHousehold = /* GraphQL */ `subscription OnUpdateHousehold($filter: ModelSubscriptionHouseholdFilterInput) {
+  onUpdateHousehold(filter: $filter) {
+    createdAt
+    householdName
+    id
+    members {
+      nextToken
+      __typename
+    }
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedSubscription<
+  APITypes.OnUpdateHouseholdSubscriptionVariables,
+  APITypes.OnUpdateHouseholdSubscription
+>;
+export const onUpdateUser = /* GraphQL */ `subscription OnUpdateUser(
+  $filter: ModelSubscriptionUserFilterInput
+  $householdID: String
+) {
+  onUpdateUser(filter: $filter, householdID: $householdID) {
+    createdAt
+    email
+    household {
+      createdAt
+      householdName
+      id
+      updatedAt
+      __typename
+    }
+    householdID
+    id
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedSubscription<
+  APITypes.OnUpdateUserSubscriptionVariables,
+  APITypes.OnUpdateUserSubscription
+>;
--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -1,11 +1,94 @@
 import type { PostConfirmationTriggerHandler } from "aws-lambda";
+import { type Schema } from "../../data/resource";
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/data";
+import { env } from "$amplify/env/post-confirmation";
+import { createUser, createHousehold } from "./graphql/mutations";
+import { listHouseholds } from "./graphql/queries";
 
+Amplify.configure(
+  {
+    API: {
+      GraphQL: {
+        endpoint: env.AMPLIFY_DATA_GRAPHQL_ENDPOINT,
+        region: env.AWS_REGION,
+        defaultAuthMode: "iam",
+      },
+    },
+  },
+  {
+    Auth: {
+      credentialsProvider: {
+        getCredentialsAndIdentityId: async () => ({
+          credentials: {
+            accessKeyId: env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+            sessionToken: env.AWS_SESSION_TOKEN,
+          },
+        }),
+        clearCredentialsAndIdentityId: () => {
+          /* noop */
+        },
+      },
+    },
+  }
+);
+
+const client = generateClient<Schema>({
+  authMode: "iam",
+});
 
 export const handler: PostConfirmationTriggerHandler = async (event) => {
   const {
     userName,
     request: { userAttributes },
   } = event;
+
+  const householdName = userAttributes["custom:householdName"] || null;
+  let householdID: string | null = null;
+
+  // Check if the household already exists
+  if (householdName) {
+    const listHouseholdsResult = await client.graphql({
+      query: listHouseholds,
+      variables: {
+        filter: {
+          householdName: {
+            eq: householdName
+          }
+        }
+      }
+    });
+
+    const existingHousehold = listHouseholdsResult.data.listHouseholds?.items[0];
+
+    if (existingHousehold) {
+      householdID = existingHousehold.id;
+    } else {
+      // Create a new household if it doesn't exist
+      const createHouseholdResult = await client.graphql({
+        query: createHousehold,
+        variables: {
+          input: {
+            householdName: householdName,
+          },
+        },
+      });
+
+      householdID = createHouseholdResult.data.createHousehold?.id || null;
+    }
+  }
+
+  // Create a new user with the householdID
+  await client.graphql({
+    query: createUser,
+    variables: {
+      input: {
+        email: event.request.userAttributes.email,
+        householdID: householdID,
+      },
+    },
+  });
 
   return event;
 };
